d3d08805a0bd3e34878652b626e91609a0bed2c2
desigana <adesigan6@gmail.com>
2024-07-08 16:42:09 +0530

    upload img gallery

--- projects/photo gallary/script.js
+++ projects/photo gallary/script.js
@@ -1,12 +1,15 @@
-
-var fullImgBox = document.getElementById("fullImgBox");
-var fullImg = document.getElementById("fullImg");
-
-function openFullImg(pic){
-    fullImgBox.style.display = "flex";
-    fullImg.src = pic;
-}
-
-function closeFullImg(){
-    fullImgBox.style.display = "none";
-}
+$(document).ready(function() {
+    $('.pic').on('click', function() {
+        let imgSrc = $(this).find('img').attr('src');
+        $('body').append(`
+            <div class="lightbox">
+                <div class="lightbox-content">
+                    <img src="${imgSrc}" alt="Lightbox Image">
+                </div>
+            </div>
+        `);
+        $('.lightbox').on('click', function() {
+            $(this).remove();
+        });
+    });
+});
